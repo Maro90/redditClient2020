aaede6f64419c57cdf23c43564ae9167aba60914
Mauro Gonzalez <mauro.gonzalez@olx.com>
2020-01-22 10:34:37 -0300

    ADD: Adding empty project to repo

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  redditClient
+//
+//  Created by Mauro Gonzalez on 22/01/2020.
+//  Copyright © 2020 mgonzalez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
